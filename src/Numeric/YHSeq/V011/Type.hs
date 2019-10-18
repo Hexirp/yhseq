4f26be3a7b139f6528e5098e958513a2c64361a8
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-19 00:01:14 +0900

    Add: index 関数の亜種を色々と
    idx 関数と同じように index 関数と index1 関数も切り出せるかも。
    そうするとインデックスが負になっていないかの判定が無駄に毎回行われて
    いたのがなくなる。

--- src/Numeric/YHSeq/V011/Type.hs
+++ src/Numeric/YHSeq/V011/Type.hs
@@ -31,3 +31,35 @@ module Numeric.YHSeq.V011.Type
       xv : xs -> if n == 0
         then xv
         else index xs (n - 1)
+
+  index1 :: [a] -> Integer -> a
+  index1 x n = if n < 1
+    then error "index1: not positive index"
+    else case x of
+      [] -> error "index1: index too large"
+      xv : xs -> if n == 1
+        then xv
+        else index xs (n - 1)
+
+  indexSeq :: Seq -> Integer -> Integer
+  indexSeq = index1
+
+  indexDPN :: DPN -> Integer -> DPNTuple
+  indexDPN = index1
+
+  indexPList :: ParentList -> Integer -> Parent
+  indexPList = index
+
+  idx :: [a] -> Integer -> a
+  idx x n = if n < 0
+    then error "idx: negative index"
+    else case x of
+      [] -> error "idx: empty list"
+      xv : xs -> idx' xv xs n
+
+  idx' :: a -> [a] -> Integer -> a
+  idx' a x n = if n == 0
+    then a
+    else case x of
+      [] -> a
+      xv : xs -> idx' xv xs (n - 1)
