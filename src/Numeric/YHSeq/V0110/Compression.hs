fd698aa6216e3fe5b4e87371cd60fe9889694a3d
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-21 18:58:19 +0900

    Add: searchParentAnc
    searchParent よりもエラーメッセージが詳細になったよ。

--- src/Numeric/YHSeq/V0110/Compression.hs
+++ src/Numeric/YHSeq/V0110/Compression.hs
@@ -33,3 +33,17 @@ module Numeric.YHSeq.V0110.Compression where
     else if mtD s p n < mtD s x n
       then p
       else searchParent' s x (p - 1)
+
+  searchParentAnc :: Seq -> Index -> Depth -> ParentIndex
+  searcnParentAnc s x n = case n `compare` 1 of
+    LT -> error "searchParentAnc: non-positive depth"
+    EQ -> error "searchParentAnc: use searchParent when depth is 1"
+    GT -> searchParentAnc' s x n x
+
+  searchParentAnc' :: Seq -> Index -> Depth -> ParentIndex -> ParentIndex
+  searchParentAnc' s x n p = case p `compare` 0 of
+    LT -> error "searchParentAnc: impossible case"
+    EQ -> error "searchParentAnc: no parent"
+    GT -> if mtD s p n < mtD s x n && p `elem` anc s x (n - 1)
+      then p
+      else searchParentAnc' s x n (p - 1)
