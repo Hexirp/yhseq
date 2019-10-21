b999fe7428997fa3871b0a3fc0eb99febccb2eca
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-21 12:39:54 +0900

    Add: mtD を試験的に実装してみる
    山を構成する関数の片割れ。

--- src/Numeric/YHSeq/V0110/Compression.hs
+++ src/Numeric/YHSeq/V0110/Compression.hs
@@ -2,3 +2,12 @@ module Numeric.YHSeq.V0110.Compression where
 
   import Prelude
   import Numeric.YHSeq.V0110.Type
+
+  mtD :: Seq -> Index -> Depth -> Diff
+  mtD s x n = case n `compare` 1 of
+    LT -> error "mtD: not positive depth"
+    EQ -> indexSeq s x
+    GT -> case mtP s x (n - 1) `compare` 0 of
+      LT -> error "mtD: irregular value of mtP"
+      EQ -> 0
+      GT -> mtD s x (n - 1) - mtD s (mtP s x (n - 1)) (n - 1)
