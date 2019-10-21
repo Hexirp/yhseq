4116392d224f90391404df05d46756162ccb12db
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-21 19:07:50 +0900

    Update: index が正しいかどうかチェックするように

--- src/Numeric/YHSeq/V0110/Compression.hs
+++ src/Numeric/YHSeq/V0110/Compression.hs
@@ -4,41 +4,50 @@ module Numeric.YHSeq.V0110.Compression where
   import Numeric.YHSeq.V0110.Type
 
   mtD :: Seq -> Index -> Depth -> Diff
-  mtD s x n = case n `compare` 1 of
-    LT -> error "mtD: non-positive depth"
-    EQ -> indexSeq s x
-    GT -> case mtP s x (n - 1) `compare` 0 of
-      LT -> error "mtD: irregular value of mtP"
-      EQ -> 0
-      GT -> mtD s x (n - 1) - mtD s (mtP s x (n - 1)) (n - 1)
+  mtD s x n = if x <= 0
+    then error "mtD: non-positive index"
+    else case n `compare` 1 of
+      LT -> error "mtD: non-positive depth"
+      EQ -> indexSeq s x
+      GT -> case mtP s x (n - 1) `compare` 0 of
+        LT -> error "mtD: irregular value of mtP"
+        EQ -> 0
+        GT -> mtD s x (n - 1) - mtD s (mtP s x (n - 1)) (n - 1)
 
   mtP :: Seq -> Index -> Depth -> ParentIndex
-  mtP s x n = case n `compare` 1 of
-    LT -> error "mtP: non-positive depth"
-    EQ -> case mtD s x n `compare` 1 of
-      LT -> error "mtP: irregular value of mtD"
-      EQ -> 0
-      GT -> searchParent s x
-    GT -> case mtD s x n `compare` 0 of
-      LT -> error "mtP: irregular value of mtD"
-      EQ -> 0
-      GT -> searchParentAnc s x n
+  mtP s x n = if x <= 0
+    then error "mtP: non-positive index"
+    else case n `compare` 1 of
+      LT -> error "mtP: non-positive depth"
+      EQ -> case mtD s x n `compare` 1 of
+        LT -> error "mtP: irregular value of mtD"
+        EQ -> 0
+        GT -> searchParent s x
+      GT -> case mtD s x n `compare` 0 of
+        LT -> error "mtP: irregular value of mtD"
+        EQ -> 0
+        GT -> searchParentAnc s x n
 
   searchParent :: Seq -> Index -> ParentIndex
-  searchParent s x = searchParent' s x (x - 1)
+  searchParent s x = if x <= 0
+    then error "searchParent: non-positive index"
+    else searchParent' s x (x - 1)
 
   searchParent' :: Seq -> Index -> ParentIndex -> ParentIndex
-  searchParent' s x p = if p <= 0
-    then error "searchParent: no parent"
-    else if mtD s p n < mtD s x n
+  searchParent' s x p = case p `compare` 0 of
+    LT -> error "searchParent: impossible case"
+    EQ -> error "searchParent: no parent"
+    GT -> if mtD s p n < mtD s x n
       then p
       else searchParent' s x (p - 1)
 
   searchParentAnc :: Seq -> Index -> Depth -> ParentIndex
-  searcnParentAnc s x n = case n `compare` 1 of
-    LT -> error "searchParentAnc: non-positive depth"
-    EQ -> error "searchParentAnc: use searchParent when depth is 1"
-    GT -> searchParentAnc' s x n x
+  searcnParentAnc s x n = if x <= 0
+    then error "searchParentAnc: non-positive index"
+    else case n `compare` 1 of
+      LT -> error "searchParentAnc: non-positive depth"
+      EQ -> error "searchParentAnc: use searchParent when depth is 1"
+      GT -> searchParentAnc' s x n x
 
   searchParentAnc' :: Seq -> Index -> Depth -> ParentIndex -> ParentIndex
   searchParentAnc' s x n p = case p `compare` 0 of
