3a6a83c34201dfd9085812806a5a5cc7e6575692
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-26 12:43:20 +0900

    Add: Decompression.hs の全体を
    このモジュールは小さいのですぐできる。

--- src/Numeric/YHSeq/V0110/Decompression.hs
+++ src/Numeric/YHSeq/V0110/Decompression.hs
@@ -3,3 +3,17 @@
 module Numeric.YHSeq.V0110.Decompression where
 
   import Numeric.YHSeq.V0110.Type
+
+  reD :: DPN -> Index -> Depth -> Diff
+  reD z x n = case n `compare` indexN z x of
+    LT -> 0
+    EQ -> indexD z x
+    GT -> reP z x (n + 1) + reD z (reP z x n) n
+
+  reP :: DPN -> Index -> Depth -> Diff
+  reP z x n = if n > indexN z x
+    then 0
+    else idx (reverse $ indexD z x) (indexN z x - n)
+
+  decompress :: DPN -> Seq
+  decompress z = map (\x -> reD z x 1) (enumFromTo 1 (lengthDPN z))
