a25cd249739e920e195040a28bab73a376cc3315
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-27 18:24:33 +0900

    Update: 引数のチェックを追加

--- src/Numeric/YHSeq/V0110/Decompression.hs
+++ src/Numeric/YHSeq/V0110/Decompression.hs
@@ -9,15 +9,19 @@ module Numeric.YHSeq.V0110.Decompression
   import Numeric.YHSeq.V0110.Type
 
   reD :: DPN -> Index -> Depth -> Diff
-  reD z x n = case n `compare` indexN z x of
-    LT -> 0
-    EQ -> indexD z x
-    GT -> reP z x (n + 1) + reD z (reP z x n) n
+  reD z x n = if x <= 0
+    then error "reD: non-positive index"
+    else case n `compare` indexN z x of
+      LT -> 0
+      EQ -> indexD z x
+      GT -> reP z x (n + 1) + reD z (reP z x n) n
 
   reP :: DPN -> Index -> Depth -> Diff
-  reP z x n = if n > indexN z x
-    then 0
-    else idx (reverse $ indexP z x) (indexN z x - n)
+  reP z x n = if x <= 0
+    then error "reP: non-positive index"
+    else if n > indexN z x
+      then 0
+      else idx (reverse $ indexP z x) (indexN z x - n)
 
   decompress :: DPN -> Seq
   decompress z = map (\x -> reD z x 1) (enumFromTo 1 (lengthDPN z))
