48ac7b3105972f28825d13598cc169d1e81bdc56
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-27 17:52:14 +0900

    Add: badPart, goodPart, cuttedPart を追加
    sliceDPN を先に実装していたおかげで簡単に作れた。

--- src/Numeric/YHSeq/V0110/Expansion.hs
+++ src/Numeric/YHSeq/V0110/Expansion.hs
@@ -6,3 +6,12 @@ module Numeric.YHSeq.V0110.Expansion where
 
   badRoot :: DPN -> Index
   badRoot z = last $ indexP z (lengthDPN z)
+
+  goodPart :: DPN -> DPN
+  goodPart z = sliceDPN 1 (badRoot z - 1) z
+
+  badPart :: DPN -> DPN
+  badPart z = sliceDPN (badRoot z) (lengthDPN z - 1)
+
+  cuttedPart :: DPN -> DPN
+  cuttedPart z = sliceDPN (lengthDPN z) (lengthDPN z)
