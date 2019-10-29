18a758076677d156b0cbf03acf18cf2c5fb30935
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-29 19:56:10 +0900

    Add: テストを hspec を使って
    少しだけ。

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+import Prelude
+import Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "V0110.fseq" $ do
+    it "expand (1,2) to (1,1,1,...)" $ do
+      fseq [1,2] 3 `shouldBe` [1,1,1]
+
+    it "expand (1,2,4,5,4) to (1,2,4,5,3,5,6,4,6,7,...)" $ do
+      fseq [1,2,4,5,4] `shouldBe` [1,2,4,5,3,5,6,4,6,7]
+
+    it "expand (1,2,4,6) to (1,2,4,5,7,8,10,...)" $ do
+      fseq [1,2,4,6] `shouldBe` [1,2,4,5,7,8,10]
+
+    it "expand (1,2,4,8,10,7) to (1,2,4,8,10,6,10,12,8,12,14,...)" $ do
+      fseq [1,2,4,8,10,7] `shouldBe` [1,2,4,8,10,6,10,12,8,12,14]
+
+    it "expand (1,2,4,8,10,8) to (1,2,4,8,10,7,12,15,11,17,21,16,23,28,...)" $ do
+      fseq [1,2,4,8,10,8] `shouldBe` [1,2,4,8,10,7,12,15,11,17,21,16,23,28]
