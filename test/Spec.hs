4fbc40755de2a813ad54d227207e9a3adb8f201b
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-29 20:53:13 +0900

    Update: qualified import を使う
    これ、併用できるのかな？

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,25 +1,25 @@
 import Prelude
 import Test.Hspec
 
-import Numeric.YHSeq.V0110 (fseq)
+import qualified Numeric.YHSeq.V0110 (fseq) as V0110
 
 main :: IO ()
 main = hspec $ do
   describe "V0110.fseq" $ do
     it "expand (1,2) to (1,1,1,...)" $ do
-      fseq [1,2] 2 `shouldBe` [1,1,1]
+      V0110.fseq [1,2] 2 `shouldBe` [1,1,1]
 
     it "expand (1,2,4,5,4) to (1,2,4,5,3,5,6,4,6,7,...)" $ do
-      fseq [1,2,4,5,4] 2 `shouldBe` [1,2,4,5,3,5,6,4,6,7]
+      V0110.fseq [1,2,4,5,4] 2 `shouldBe` [1,2,4,5,3,5,6,4,6,7]
 
     it "expand (1,2,4,6) to (1,2,4,5,7,8,10,...)" $ do
-      fseq [1,2,4,6] 2 `shouldBe` [1,2,4,5,7,8,10]
+      V0110.fseq [1,2,4,6] 2 `shouldBe` [1,2,4,5,7,8,10]
 
     it "expand (1,2,4,8,9,8) to (1,2,4,8,9,7,12,13,11,17,18,...)" $ do
-      fseq [1,2,4,8,9,8] 2 `shouldBe` [1,2,4,8,9,7,12,13,11,17,18]
+      V0110.fseq [1,2,4,8,9,8] 2 `shouldBe` [1,2,4,8,9,7,12,13,11,17,18]
 
     it "expand (1,2,4,8,10,7) to (1,2,4,8,10,6,10,12,8,12,14,...)" $ do
-      fseq [1,2,4,8,10,7] 2 `shouldBe` [1,2,4,8,10,6,10,12,8,12,14]
+      V0110.fseq [1,2,4,8,10,7] 2 `shouldBe` [1,2,4,8,10,6,10,12,8,12,14]
 
     it "expand (1,2,4,8,10,8) to (1,2,4,8,10,7,12,15,11,17,21,...)" $ do
-      fseq [1,2,4,8,10,8] 2 `shouldBe` [1,2,4,8,10,7,12,15,11,17,21]
+      V0110.fseq [1,2,4,8,10,8] 2 `shouldBe` [1,2,4,8,10,7,12,15,11,17,21]
