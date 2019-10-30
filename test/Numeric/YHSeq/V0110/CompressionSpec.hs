e5fd3edf48bc3694e4741237c6f7dc1e2764b64e
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-10-30 21:50:05 +0900

    Update: インポートリストを弄ったのを反映してなかった

--- test/Numeric/YHSeq/V0110/CompressionSpec.hs
+++ test/Numeric/YHSeq/V0110/CompressionSpec.hs
@@ -13,39 +13,39 @@ module Numeric.YHSeq.V0110.CompressionSpec (spec) where
     describe "mtD" $ do
 
       it "is D = 1 at S = (1,2), x = 1, and n = 1" $ do
-        V0110.mtD [1,2] 1 1 `shouldBe` 1
+        mtD [1,2] 1 1 `shouldBe` 1
 
       it "is D = 2 at S = (1,2), x = 2, and n = 1" $ do
-        V0110.mtD [1,2] 2 1 `shouldBe` 2
+        mtD [1,2] 2 1 `shouldBe` 2
 
       it "is D = 0 at S = (1,2), x = 1, and n = 2" $ do
-        V0110.mtD [1,2] 1 2 `shouldBe` 0
+        mtD [1,2] 1 2 `shouldBe` 0
 
       it "is D = 1 at S = (1,2), x = 2, and n = 2" $ do
-        V0110.mtD [1,2] 2 2 `shouldBe` 1
+        mtD [1,2] 2 2 `shouldBe` 1
 
       it "is D = 0 at S = (1,2), x = 1, and n = 3" $ do
-        V0110.mtD [1,2] 1 3 `shouldBe` 0
+        mtD [1,2] 1 3 `shouldBe` 0
 
       it "is D = 1 at S = (1,2), x = 2, and n = 3 and is broken" $ do
-        V0110.mtD [1,2] 2 3 `shouldBe` 1
+        mtD [1,2] 2 3 `shouldBe` 1
 
     describe "mtP" $ do
 
       it "is P = 0 at S = (1,2), x = 1, and n = 1" $ do
-        V0110.mtP [1,2] 1 1 `shouldBe` 0
+        mtP [1,2] 1 1 `shouldBe` 0
 
       it "is P = 1 at S = (1,2), x = 2, and n = 1" $ do
-        V0110.mtP [1,2] 2 1 `shouldBe` 1
+        mtP [1,2] 2 1 `shouldBe` 1
 
       it "is P = 0 at S = (1,2), x = 1, and n = 2" $ do
-        V0110.mtP [1,2] 1 2 `shouldBe` 0
+        mtP [1,2] 1 2 `shouldBe` 0
 
       it "is P = 1 at S = (1,2), x = 2, and n = 2" $ do
-        V0110.mtP [1,2] 2 2 `shouldBe` 1
+        mtP [1,2] 2 2 `shouldBe` 1
 
       it "is P = 0 at S = (1,2), x = 1, and n = 3" $ do
-        V0110.mtP [1,2] 1 3 `shouldBe` 0
+        mtP [1,2] 1 3 `shouldBe` 0
 
       it "is P = 1 at S = (1,2), x = 2, and n = 3 and is broken" $ do
-        V0110.mtP [1,2] 2 3 `shouldBe` 1
+        mtP [1,2] 2 3 `shouldBe` 1
