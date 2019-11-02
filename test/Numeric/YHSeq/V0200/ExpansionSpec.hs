fbf03b53c8b00566d910e3f006b77215fc598209
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-02 23:04:39 +0900

    Add: V0200.Expansion のテストを

---
+++ test/Numeric/YHSeq/V0200/ExpansionSpec.hs
@@ -0,0 +1,16 @@
+module Numeric.YHSeq.V0200.ExpansionSpec (spec) where
+
+  import Prelude hiding (length)
+
+  import Test.Hspec
+
+  import Numeric.YHSeq.V0200.Type
+  import Numeric.YHSeq.V0200.Expansion
+
+  spec :: Spec
+  spec = do
+
+    describe "goodPart" $ do
+
+      it "is [1] at [1,2,4]" $ do
+        goodPart [1,2,4] `shouldBe` [1]
