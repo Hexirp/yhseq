e4c4a3738f275075ed65916e13a2bbcf1229ebf6
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-02 13:04:31 +0900

    Update: エラーメッセージ内のバージョンを上げる

--- src/Numeric/YHSeq/V0200/Expansion.hs
+++ src/Numeric/YHSeq/V0200/Expansion.hs
@@ -111,4 +111,4 @@ module Numeric.YHSeq.V0200.Expansion
   expand z c n = case c `compare` 1 of
     LT -> error "expand: non-positive class"
     EQ -> goodPart z ++ concat (map (\m -> copiedBadPart z m) $ enumFromTo 0 n)
-    GT -> error "expand: is undefined at 0.1.1.0 when class is greater than 1"
+    GT -> error "expand: is undefined at 0.2.0.0 when class is greater than 1"
