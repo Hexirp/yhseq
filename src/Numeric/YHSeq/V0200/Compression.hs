b6dc151c20f9349f1bd46d1dc25e37770ffaabed
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-02 11:42:36 +0900

    Fix: 親の節点が指す値が 0 のとき mtD を 0 にする
    [1], [2] に記載されている Y数列 Hexirp 版 のバグを修正した。
    
    [1]: https://github.com/Hexirp/yhseq/issues/1
    [2]: https://googology.wikia.org/ja/wiki/%E3%83%A6%E3%83%BC%E3%82%B6%E3%83%BC%E3%83%96%E3%83%AD%E3%82%B0:Hexirp/(2019-10-30)_Y%E6%95%B0%E5%88%97_Hexirp_%E7%89%88%E3%81%AE%E3%83%90%E3%82%B0%E3%81%8C%E7%99%BA%E8%A6%8B

--- src/Numeric/YHSeq/V0200/Compression.hs
+++ src/Numeric/YHSeq/V0200/Compression.hs
@@ -34,7 +34,10 @@ module Numeric.YHSeq.V0200.Compression
     GT -> case mtP s x (n - 1) `compare` 0 of
       LT -> error "mtD: irregular value of mtP"
       EQ -> 0
-      GT -> mtD s x (n - 1) - mtD s (mtP s x (n - 1)) (n - 1)
+      GT -> case mtD s (mtP s x (n - 1)) (n - 1) of
+        LT -> error "mtD: irregular value of mtD"
+        EQ -> 0
+        GT -> mtD s x (n - 1) - mtD s (mtP s x (n - 1)) (n - 1)
 
   mtP :: Seq -> Index -> Depth -> ParentIndex
   mtP s x n = if x <= 0
