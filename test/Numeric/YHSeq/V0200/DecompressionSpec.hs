ee193622a945555d1994fef196a1faeec06587fb
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-02 23:38:32 +0900

    Update: decompress の逆関数のテストを増やす

--- test/Numeric/YHSeq/V0200/DecompressionSpec.hs
+++ test/Numeric/YHSeq/V0200/DecompressionSpec.hs
@@ -13,5 +13,14 @@ module Numeric.YHSeq.V0200.DecompressionSpec (spec) where
 
     describe "decompress" $ do
 
+      it "is a inverse function of `compress` at (1,2,4,5,4)" $ do
+        decompress (compress [1,2,4,5,4]) `shouldBe` [1,2,4,5,4]
+
+      it "is a inverse function of `compress` at (1,2,4,6)" $ do
+        decompress (compress [1,2,4,6]) `shouldBe` [1,2,4,6]
+
+      it "is a inverse function of `compress` at (1,2,4,8)" $ do
+        decompress (compress [1,2,4,8]) `shouldBe` [1,2,4,8]
+
       it "is a inverse function of `compress` at (1,2,4,8,10,8)" $ do
         decompress (compress [1,2,4,8,10,8]) `shouldBe` [1,2,4,8,10,8]
