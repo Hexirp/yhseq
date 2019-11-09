175d9b992204ba06c2432018bff83539ee20ffe8
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-09 20:08:34 +0900

    Update: フォーマットする
    考えを変えた。

--- app/YHSeq.hs
+++ app/YHSeq.hs
@@ -2,12 +2,13 @@ module YHSeq where
 
   import Prelude
 
-  data Arguments = Arguments {
-    version :: String ,
-    seq     :: String ,
-    num     :: String ,
-    detail  :: Bool ,
-    force   :: Bool }
+  data Arguments = Arguments
+    { version :: String
+    , seq     :: String
+    , num     :: String
+    , detail  :: Bool
+    , force   :: Bool
+    }
 
   yhseq :: Arguments -> IO ()
   yhseq = return ()
