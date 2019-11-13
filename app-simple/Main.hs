3c4ad46590d24c5672213dfc7a9cfcdb02f0013c
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-13 23:27:49 +0900

    Update: さらに高級にする
    app/ と同様の流れを想定している。

--- app-simple/Main.hs
+++ app-simple/Main.hs
@@ -2,12 +2,33 @@ module Main where
 
   import Prelude
   import System.Environment (getArgs)
+  import System.Exception (ErrorCall, try, evaluate, throwIO)
 
   import Numeric.YHSeq.V0210 (fseq)
+
+  type OpArg = (String, String)
+
+  type Arg = ([Integer], Integer)
 
   main :: IO ()
   main = do
     args <- getArgs
-    case args of
-      [seq, num] -> print $ fseq (read seq) (read num)
-      _ -> error "The arguments are incorrect!"
+    opArg <- optparse args
+    arg <- parse opArg
+    e <- (try $ calc arg) :: IO (Either ErrorCall [Integer])
+    case e of
+      Left ex -> throwIO ex
+      Right s' -> print s'
+
+  optparse :: [String] -> IO OpArg
+  optparse [seq, num] = return (seq, num)
+  optparse _          = evaluate $ error "The arguments is incorrect!"
+
+  parse :: OpArg -> IO Arg
+  parse (seq, num) = do
+    s <- evaluate $ (read seq :: [Integer])
+    n <- evaluate $ (read num :: Integer)
+    return $ (s, n)
+
+  calc :: Arg -> IO [Integer]
+  calc (seq, num) = evaluate $ fseq seq num
