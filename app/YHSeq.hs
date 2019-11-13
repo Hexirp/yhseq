1e254af3424a2f76d43627bd089d1675fcba5843
Hexirp Hexirp@users.noreply.github.com <Hexirp@users.noreply.github.com>
2019-11-13 22:25:32 +0900

    Update: YHSeq の内容を型の定義へ

--- app/YHSeq.hs
+++ app/YHSeq.hs
@@ -2,14 +2,17 @@ module YHSeq where
 
   import Prelude
 
-  -- YHSeq + Arguments
-  data YHSA = YHSA
-    { yhsv :: String -- version
-    , yhss :: String -- sequence
-    , yhsn :: String -- number
-    , yhsd :: Bool -- detail
-    , yhsf :: Bool -- force
-    }
+  -- | Optparsed arguments.
+  data OpArg = OpArg String OpSeqNum Bool Bool
 
-  yhseq :: YHSA -> IO ()
-  yhseq (YHSV yhsv yhss yhsn yhsd yhsf) = return ()
+  -- | Optparsed sequence and number.
+  data OpSeqNum = Spr String String | Glu String
+
+  -- | Implemented versions of Y-sequence Hexirp edition.
+  data Ver = V0110 | V0111 | V0200 | V0210
+
+  -- | Arguments.
+  data Arg = Arg Ver SeqNum Bool Bool
+
+  -- | Sequence and Number.
+  data SeqNum = SeqNum [Integer] Integer
