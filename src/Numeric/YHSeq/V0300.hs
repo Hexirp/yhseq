67951a36604dcb11f24cbd02ab8e7c693c57ab89
Hexirp <Hexirp@users.noreply.github.com>
2020-07-03 21:18:47 +0900

    Update: 実行速度を上げる試み

--- src/Numeric/YHSeq/V0300.hs
+++ src/Numeric/YHSeq/V0300.hs
@@ -1,55 +1,18 @@
-module Numeric.YHSeq.V0300
-  ( Cofinal (IsZero, IsSucc, IsLimit)
-  , isZero
-  , isSucc
-  , isLimit
-  , scof
-  , fseq
-  , pseq
-  , yhseq
-  ) where
+module Numeric.YHSeq.V0300 where
 
-  import Prelude hiding (length)
+  import Prelude
 
-  import Numeric.YHSeq.V0300.Type
-  import Numeric.YHSeq.V0300.Compression   (seqClass, compress)
-  import Numeric.YHSeq.V0300.Expansion     (expand)
-  import Numeric.YHSeq.V0300.Decompression (decompress)
+  import           Data.IntSet      ( IntSet )
+  import qualified Data.IntSet as S
+  import           Data.Vector      ( Vector )
+  import qualified Data.Vector as V
 
-  data Cofinal = IsZero | IsSucc | IsLimit
+  -- 数列
+  newtype Sequence = Sequence { unSequence :: Vector Int }
 
-  isZero :: Seq -> Bool
-  isZero s = s == []
-
-  isSucc :: Seq -> Bool
-  isSucc s = indexSeq s (lengthSeq s) == 1
-
-  isLimit :: Seq -> Bool
-  isLimit s = not (isZero s) && not (isSucc s)
-
-  -- sequence cofinality
-  scof :: Seq -> Cofinal
-  scof s = case s of
-    []    -> IsZero
-    _ : _ -> if last s == 1
-      then IsSucc
-      else IsLimit
-
-  -- fundamental sequence
-  fseq :: Seq -> Integer -> Seq
-  fseq s n = if isLimit s
-    then decompress (expand (compress s) (seqClass s) n)
-    else error "fseq: it is not limit sequence"
-
-  -- predecessor sequence
-  pseq :: Seq -> Seq
-  pseq s = if isSucc s
-    then dropLast s
-    else error "pseq: it is not successor sequence"
-
-  -- | It's a very very large function.
-  yhseq :: Seq -> Integer -> Integer
-  yhseq s n = case scof s of
-    IsZero  -> n
-    IsSucc  -> yhseq (pseq s) (n + 1)
-    IsLimit -> yhseq (fseq s n) (n + 1)
+  -- 山
+  data Mountain = Mountain
+    { difference :: Vector (Vector Int)
+    , parent     :: Vector (Vector Int)
+    , ancestor   :: Vector (Vector IntSet)
+    }
