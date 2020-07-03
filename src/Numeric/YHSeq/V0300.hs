6daf35e87e572543bbb59e7be5f6d88e7084b81a
Hexirp <Hexirp@users.noreply.github.com>
2020-07-03 21:34:20 +0900

    Update: メモ化に挑戦する

--- src/Numeric/YHSeq/V0300.hs
+++ src/Numeric/YHSeq/V0300.hs
@@ -18,8 +18,12 @@ module Numeric.YHSeq.V0300 where
     }
 
   fromSeqToMt :: Sequence -> Mountain
-  fromSeqToMt (Sequence s) = let len_s = V.length s in Mountain
-    { difference = generate len_s (\n -> generate len_s (\x -> diffs n x s))
-    , parent     = generate len_s (\n -> generate len_s (\x -> paets n x s))
-    , ancestor   = generate len_s (\n -> generate len_s (\x -> ances n x s))
-    }
+  fromSeqToMt (Sequence s) = let len_s = V.length s in
+    let z = Mountain
+      { difference = generate len_s (\n -> generate len_s (\x -> diffs z s n x))
+      , parent     = generate len_s (\n -> generate len_s (\x -> paets z s n x))
+      , ancestor   = generate len_s (\n -> generate len_s (\x -> ances z s n x))
+      }
+
+  diffs :: Mountain -> Sequence -> Int -> Int -> Int
+  diffs m s n x = undefined
