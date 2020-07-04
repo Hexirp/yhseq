32b3732f3d9b3bd4cd2828cb8db3fba423c9d889
Hexirp <Hexirp@users.noreply.github.com>
2020-07-04 10:34:38 +0900

    Update: fromSeqToMt を大きく進める

--- src/Numeric/YHSeq/V0300.hs
+++ src/Numeric/YHSeq/V0300.hs
@@ -8,22 +8,61 @@ module Numeric.YHSeq.V0300 where
   import qualified Data.Vector as V
 
   -- 数列
-  newtype Sequence = Sequence { unSequence :: Vector Int }
+  newtype Sequence = Sequence { unSeq :: Vector Int }
 
   -- 山
   data Mountain = Mountain
-    { difference :: Vector (Vector Int)
-    , parent     :: Vector (Vector Int)
-    , ancestor   :: Vector (Vector IntSet)
+    { -- 階差
+      diff :: Vector (Vector Int)
+    , -- 親の情報
+      paet :: Vector (Vector Int)
+    , -- 先祖の情報
+      ance :: Vector (Vector IntSet)
     }
 
-  fromSeqToMt :: Sequence -> Mountain
-  fromSeqToMt (Sequence s) = let len_s = V.length s in
-    let z = Mountain
-      { difference = generate len_s (\n -> generate len_s (\x -> diffs z s n x))
-      , parent     = generate len_s (\n -> generate len_s (\x -> paets z s n x))
-      , ancestor   = generate len_s (\n -> generate len_s (\x -> ances z s n x))
-      }
+  -- 山から階差を得る
+  diffz :: Mountain -> Int -> Int -> Int
+  diffz z x n = (diff z ! x) ! n
 
-  diffs :: Mountain -> Sequence -> Int -> Int -> Int
-  diffs m s n x = undefined
+  -- 山から親の情報を得る
+  paetz :: Mountain -> Int -> Int -> Int
+  paetz z x n = (paet z ! x) ! n
+
+  -- 山から先祖の情報を得る
+  ancez :: Mountain -> Int -> Int -> IntSet
+  ancez z x n = (ance z ! x) ! n
+
+  -- 数列から山を構築する
+  fromSeqToMt :: Sequence -> Mountain
+  fromSeqToMt s =
+    let
+      diffs :: Mountain -> Sequence -> Int -> Int -> Int
+      diffs z s x n = case n `compare` 1 of
+        LT -> undefined
+        EQ -> unSeq s S.! x
+        GT -> case paetz z x (n - 1) `compare` 0 of
+          LT -> undefined
+          EQ -> 0
+          GT -> case diffz z (paetz z x (n - 1)) (n - 1) of
+            LT -> undefined
+            EQ -> 0
+            GT -> diffz z x (n - 1) - diffz z (paetz z x (n - 1)) (n - 1)
+      paets :: Mountain -> Int -> Int -> Int
+      paets z x n = paets' z x n (x - 1)
+      paets' :: Mountain -> Int -> Int -> Int -> Int
+      paets' z x n p = case p `compare` 0 of
+        LT -> undefined
+        EQ -> 0
+        GT -> if diffz z p n < diffz z x n && in_ances z x n p
+      ances :: Mountain -> Int -> Int -> IntSet
+      ances z x n = case x `compare` 0 of
+        LT -> undefined
+        EQ -> S.empty
+        GT -> S.insert x (ances z (paets z x n) n)
+      in_ances :: Mountain -> Int -> Int -> Bool
+      in_ances z x n p = case n `compare` 1 of
+        LT -> undefined
+        EQ -> True
+        GT -> S.member p (ances s x (n - 1))
+    in
+      undefined
