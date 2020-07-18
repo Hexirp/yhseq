3f68fe26b2d82aeb74cb0ae40814271545dbdea9
Hexirp <Hexirp@users.noreply.github.com>
2020-07-18 23:26:25 +0900

    Update: app/Main.hs の内容をスマートにする

--- app/Main.hs
+++ app/Main.hs
@@ -17,20 +17,20 @@ module Main where
     args <- getArgs
     opArg <- optparse args
     arg <- parse opArg
-    res <- calc arg `catch` \e -> const id (e :: ErrorCall) $ do
-      putStrLn "yhseq-simple: Something happened!"
+    res <- calc arg `catch` \e -> let _e = e :: ErrorCall in do
+      putStrLn "yhseq: Something happened!"
       throwIO e
     print res
 
   optparse :: [String] -> IO OpArg
   optparse [seq, num] = return (seq, num)
-  optparse _          = evaluate $ error "The arguments is incorrect!"
+  optparse _          = evaluate (error "yhseq: The arguments is incorrect!")
 
   parse :: OpArg -> IO Arg
   parse (seq, num) = do
-    s <- evaluate $ (read seq :: [Integer])
-    n <- evaluate $ (read num :: Integer)
-    return $ (s, n)
+    s <- evaluate (read seq :: [Integer])
+    n <- evaluate (read num :: Integer)
+    return (s, n)
 
   calc :: Arg -> IO [Integer]
-  calc (seq, num) = evaluate $ fseq seq num
+  calc (seq, num) = evaluate (fseq seq num)
