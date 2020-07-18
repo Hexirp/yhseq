bde7beacbf5ff25b1b8aaf23d621f38430b6df2a
Hexirp <Hexirp@users.noreply.github.com>
2020-07-19 00:17:15 +0900

    Add: V0201 を

---
+++ src/Numeric/YHSeq/V0201.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- {-# LANGUAGE DerivingVia #-}
+
+-- | YH数列システム 2.0.1 の定義。
+--
+-- ここでは "Data.IntSet" と "Data.Vector" を活用している。
+module Numeric.YHSeq.V0201 where
+
+  import Prelude
+
+  import           Data.IntSet      ( IntSet )
+  import qualified Data.IntSet as S
+  import           Data.Vector      ( Vector )
+  import qualified Data.Vector as V
+
+  -- | 数列、または列。
+  newtype Sequence = Sequence { unSequence :: Vector Int }
+    deriving stock (Eq, Ord, Show, Read)
+    deriving newtype (Semigroup, Monoid)
+
+  -- | 'Sequence' のための添字演算。
+  ixS :: Sequence -> Int -> Int
+  ixS s x = unSeq s V.! (x - 1)
+
+  -- | 階差。
+  newtype Difference = Difference { unDifference :: Int }
+    deriving stock (Eq, Ord, Enum, Bounded)
+    deriving newtype (Show, Read, Num, Real, Integral, Semigroup, Monoid)
+
+  -- | 添字。
+  newtype Index = Index { unIndex :: Int }
+    deriving stock (Eq, Ord, Enum, Bounded)
+    deriving newtype (Show, Read, Num, Real, Integral, Semigroup, Monoid)
+
+  -- | 添字の集合。
+  newtype IndexSet = IndexSet { unIndexSet :: IntSet }
+    deriving stock (Eq, Ord)
+    deriving newtype (Show, Read, Semigroup, Monoid)
+
+  -- | 山。
+  --
+  -- 条件として dMt と pMt と aMt の長さはそれぞれ sMt に等しい。
+  data Mountain = Mountain
+    { -- | 山のサイズ。
+      sMt :: Int
+    , -- | 階差。
+      dMt :: Vector (Vector Difference)
+    , -- | 親の添字。
+      pMt :: Vector (Vector Index)
+    , -- | 先祖の集合。
+      pMt :: Vector (Vector IndexSet)
+    } deriving (Eq, Ord, Show, Read)
