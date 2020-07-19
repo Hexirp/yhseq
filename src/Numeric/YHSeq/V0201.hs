5b6ad1911992946fd95d4e418a45959d4c23b95d
Hexirp <Hexirp@users.noreply.github.com>
2020-07-20 02:37:38 +0900

    Update: 文書の情報を増やす

--- src/Numeric/YHSeq/V0201.hs
+++ src/Numeric/YHSeq/V0201.hs
@@ -5,6 +5,8 @@
 -- | YH数列システム 2.0.1 の定義。
 --
 -- ここでは "Data.IntSet" と "Data.Vector" を活用している。
+--
+-- 英語では YH sequence system 2.0.1 と呼ぶことにする。短く呼ぶときは YHSS 2.0.1 と呼ぶことにする。
 module Numeric.YHSeq.V0201 where
 
   import Prelude
@@ -26,18 +28,24 @@ module Numeric.YHSeq.V0201 where
   ixS s x = unSeq s V.! (x - 1)
 
   -- | 階差。
+  --
+  -- 数列の階差を取った列を階差列と呼ぶことにする。たとえば (1,2,4,8,16,...) の階差列は (0,1,2,4,8,...) だ。
   newtype Difference = Difference { unDifference :: Int }
     deriving stock (Eq, Ord, Bounded)
     deriving newtype (Enum, Show, Read, Num, Real, Integral)
     deriving (Semigroup, Monoid) via Sum Int
 
   -- | 添字。
+  --
+  -- 添字といっても 'Sequence' の添字ではなく親を参照したりするときに使う添字だ。 'ixS' を見てもらえば分かるように、 'Sequence' の添字は普通の 'Int' だ。
   newtype Index = Index { unIndex :: Int }
     deriving stock (Eq, Ord, Bounded)
     deriving newtype (Enum, Show, Read, Num, Real, Integral)
     deriving (Semigroup, Monoid) via Sum Int
 
   -- | 添字の集合。
+  --
+  -- 添字の集合と言っても、たぶん先祖の集合を表すのしか使わないと思う。
   newtype IndexSet = IndexSet { unIndexSet :: IntSet }
     deriving stock (Eq, Ord)
     deriving newtype (Show, Read, Semigroup, Monoid)
