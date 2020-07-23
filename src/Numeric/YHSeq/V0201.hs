eb00c6645c885276f6add24f4e5185bf312de02e
Hexirp <Hexirp@users.noreply.github.com>
2020-07-23 17:17:32 +0900

    Fix: 変更忘れ

--- src/Numeric/YHSeq/V0201.hs
+++ src/Numeric/YHSeq/V0201.hs
@@ -88,7 +88,7 @@ module Numeric.YHSeq.V0201 where
   calcDiffOnMtWiM :: Mountain -> Sequence -> Index -> Depth -> Difference
   calcDiffOnMtWiM z s x n = case n `compare` 1 of
     LT -> undefined
-    EQ -> Difference (s `ixS` x)
+    EQ -> Difference (s `ixSeq` x)
     GT -> case ixMtToPaet z x (n - 1) `compare` 0 of
       LT -> undefined
       EQ -> 0
