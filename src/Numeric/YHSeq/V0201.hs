e3d1ab4551eccd2a761d4b9065215088f5794989
Hexirp <Hexirp@users.noreply.github.com>
2020-07-23 16:54:23 +0900

    Add: 深さを

--- src/Numeric/YHSeq/V0201.hs
+++ src/Numeric/YHSeq/V0201.hs
@@ -45,10 +45,18 @@ module Numeric.YHSeq.V0201 where
 
   -- | 添字の集合。
   --
-  -- 添字の集合と言っても、たぶん先祖の集合を表すのしか使わないと思う。
+  -- 添字の集合と言っても、たぶん先祖の集合を表すのにしか使わないと思う。
   newtype IndexSet = IndexSet { unIndexSet :: IntSet }
     deriving stock (Eq, Ord)
     deriving newtype (Show, Read, Semigroup, Monoid)
+
+  -- | 深さ。
+  --
+  -- YHSS 2.0.1 では、深さは 0 にならない。
+  newtype Depth = Depth { unDepth :: Int }
+    deriving stock (Eq, Ord, Bounded)
+    deriving newtype (Enum, Show, Read, Num, Real, Integral)
+    deriving (Semigroup, Monoid) via Sum Int
 
   -- | 山。
   --
@@ -65,16 +73,16 @@ module Numeric.YHSeq.V0201 where
     } deriving (Eq, Ord, Show, Read)
 
   -- | 山から階差を添字で取得する。
-  ixMtDiff :: Mountain -> Int -> Int -> Difference
-  ixMtDiff z x n = dMt z V.! (x - 1) V.! (n - 1)
+  ixMtDiff :: Mountain -> Index -> Depth -> Difference
+  ixMtDiff z x n = dMt z V.! (unIndex x - 1) V.! (unDepth n - 1)
 
   -- | 山から親の添字を添字で取得する。
-  ixMtPaet :: Mountain -> Int -> Int -> Index
-  ixMtPaet z x n = pMt z V.! (x - 1) V.! (n - 1)
+  ixMtPaet :: Mountain -> Index -> Depth -> Index
+  ixMtPaet z x n = pMt z V.! (unIndex x - 1) V.! (unDepth n - 1)
 
   -- | 山から先祖の集合を添字で取得する。
-  ixMtAnce :: Mountain -> Int -> Int -> IndexSet
-  ixMtAnce z x n = aMt z V.! (x - 1) V.! (n - 1)
+  ixMtAnce :: Mountain -> Index -> Depth -> IndexSet
+  ixMtAnce z x n = aMt z V.! (unIndex x - 1) V.! (unDepth n - 1)
 
   -- | メモを参照しながら山の階差を計算する。
   csMtDiffWiM :: Mountain -> Sequence -> Int -> Int -> Difference
