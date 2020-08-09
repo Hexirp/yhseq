59ae4817e9206977c3747c57ad03c310216a6ece
Hexirp <Hexirp@users.noreply.github.com>
2020-08-10 01:02:18 +0900

    Fix: 名前の変更に追随する

--- test/Numeric/YHSeq/V0201Spec.hs
+++ test/Numeric/YHSeq/V0201Spec.hs
@@ -15,17 +15,17 @@ module Numeric.YHSeq.V0201Spec (spec) where
   spec = runIO $ do
     print "calcMt $ Sequence $ V.fromList $ [1,2,4,8,10,8]"
     print (calcMt $ Sequence $ V.fromList $ [1,2,4,8,10,8])
-    print "mtClass $ calcMtFromList []"
-    print (mtClass $ calcMtFromList [])
-    print "mtClass $ calcMtFromList [1,2,1]"
-    print (mtClass $ calcMtFromList [1,2,1])
-    print "mtClass $ calcMtFromList [1,2,4,8,10,8]"
-    print (mtClass $ calcMtFromList [1,2,4,8,10,8])
-    print "mtClass $ calcMtFromList [1,3]"
-    print (mtClass $ calcMtFromList [1,3])
-    print "mtClass $ calcMtFromList [1,3,4,2,5,6,5]"
-    print (mtClass $ calcMtFromList [1,3,4,2,5,6,5])
-    print "mtClass $ calcMtFromList [1,11]"
-    print (mtClass $ calcMtFromList [1,11])
+    print "calcCofType $ calcMtFromList []"
+    print (calcCofType $ calcMtFromList [])
+    print "calcCofType $ calcMtFromList [1,2,1]"
+    print (calcCofType $ calcMtFromList [1,2,1])
+    print "calcCofType $ calcMtFromList [1,2,4,8,10,8]"
+    print (calcCofType $ calcMtFromList [1,2,4,8,10,8])
+    print "calcCofType $ calcMtFromList [1,3]"
+    print (calcCofType $ calcMtFromList [1,3])
+    print "calcCofType $ calcMtFromList [1,3,4,2,5,6,5]"
+    print (calcCofType $ calcMtFromList [1,3,4,2,5,6,5])
+    print "calcCofType $ calcMtFromList [1,11]"
+    print (calcCofType $ calcMtFromList [1,11])
     print "calcDpn $ calcMtFromList [1,2,3,8,10,8]"
     print (calcDpn $ calcMtFromList [1,2,3,8,10,8])
